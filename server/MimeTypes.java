a60401a622c570bcc7fa63db82ccc1b9d5fc918a
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-04 12:37:42 -0800

    Added basic idea for mimetypes

---
+++ server/MimeTypes.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class MimeTypes {
+    public static HashMap<String, String> mimeTypes = new HashMap<>();
+    /*
+    Should we hard-initiate the hashmap with the mimetypes
+    or should be read off the "mime.types" file and create the Hashmap?
+     */
+}
