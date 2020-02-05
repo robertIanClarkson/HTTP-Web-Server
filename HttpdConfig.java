1ad1f21f459d31926e768f1431dcd1f3493ca157
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-04 20:01:03 -0800

    created "HttpdConfig" which strips all data from "conf/httpd.conf"

---
+++ HttpdConfig.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HttpdConfig {
+
+    private BufferedReader reader;
+
+    public String serverRoot, documentRoot, listen, logFile, scriptAlias, aliasA, aliasB;
+
+    HttpdConfig( String fileName ) {
+        try {
+            reader = new BufferedReader(new FileReader( fileName ));
+            serverRoot = stripQuotes(stripData(reader.readLine()));
+            documentRoot = stripQuotes(stripData(reader.readLine()));
+            listen = stripData(reader.readLine());
+            logFile = stripQuotes(stripData(reader.readLine()));
+            scriptAlias = stripQuotes(stripData(reader.readLine()));
+            aliasA = stripQuotes(stripData(reader.readLine()));
+            aliasB = stripQuotes(stripData(reader.readLine()));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private String stripQuotes( String line ) {
+        int x = line.indexOf("\"");
+        int y = line.lastIndexOf("\"");
+        return line.substring( x+1, y);
+    }
+
+    private String stripData(String line) {
+        int x = line.indexOf(" ");
+        return line.substring(x+1);
+    }
+}
