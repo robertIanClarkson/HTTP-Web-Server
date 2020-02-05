b7972acdf0c431ff882394ce9f60c77d3e551f35
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-05 00:59:24 -0800

    mimetypes read from file then stored into a hashmap

--- MimeConfig.java
+++ MimeConfig.java
@@ -1,6 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MimeConfig {
-    MimeConfig(String fileName) {
 
+    private HashMap<String, String> mimeTypes;
+
+    MimeConfig(String fileName) {
+        BufferedReader reader;
+        String line;
+        String type;
+        String[] extensions;
+        mimeTypes = new HashMap<>();
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            while ((line = reader.readLine()) != null) {
+                if ((!line.contains("#")) && (line.length() != 0) && (line.contains("\t"))) {
+                    type = getType(line);
+                    extensions = getExtensions(line);
+                    for (String extension : extensions) {
+                        mimeTypes.put(extension, type);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("MimeConfig --> IOException: " + e);
+        }
     }
-}
+
+    private String getType(String line) {
+        return line.substring(0, line.indexOf("\t"));
+    }
+
+    private String[] getExtensions(String line) {
+        String right = line.substring(line.lastIndexOf("\t") + 1);
+        return right.split(" ");
+    }
+ }
