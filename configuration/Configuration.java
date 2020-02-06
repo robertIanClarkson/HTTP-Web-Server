e62b0b09abf20f0fd8a396a633aa37a1a385d178
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-05 20:44:31 -0800

    Made "Configuration.java" constructor public

--- configuration/Configuration.java
+++ configuration/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
     private HttpdConfig httpd;
     private MimeConfig mime;
 
-    Configuration(String httpdFile, String mimeFile) {
+    public Configuration(String httpdFile, String mimeFile) {
         httpd = new HttpdConfig(httpdFile);
         mime = new MimeConfig(mimeFile);
     }
