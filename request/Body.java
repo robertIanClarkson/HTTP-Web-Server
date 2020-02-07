b3bd1c8d949ae22e51ac6b7c70ed2785af0e3ceb
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-07 15:26:44 -0800

    Body extracts length from Content-Length

--- request/Body.java
+++ request/Body.java
@@ -1,14 +1,17 @@
 package request;
-
+import configuration.headers.ContentLength;
 import configuration.headers.Header;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class Body {
 
     private int length;
 
-    public Body(Socket client, Header header) {
-
+    public Body(Socket client, ContentLength header) throws IOException {
+        length = header.getLength();
     }
 }
