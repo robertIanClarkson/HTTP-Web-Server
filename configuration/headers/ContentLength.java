82ac6c41ed30b0fea6e10e908f9577d052f44115
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-07 15:23:57 -0800

    Added getlength()

--- configuration/headers/ContentLength.java
+++ configuration/headers/ContentLength.java
@@ -1,7 +1,14 @@
 package configuration.headers;
 
 public class ContentLength extends Header {
+    private int length;
     public void init(String data) {
-
+        length = Integer.parseInt(data);
     }
+
+    public int getLength() {
+        return length;
+    }
+
+
 }
