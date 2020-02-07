a95b6070f7d84c08e281399e14f2b8df236edf72
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-07 15:24:43 -0800

    added getHost()

--- configuration/headers/Host.java
+++ configuration/headers/Host.java
@@ -1,7 +1,12 @@
 package configuration.headers;
 
 public class Host extends Header {
+    private String host;
     public void init(String data) {
+        host = data;
+    }
 
+    public String getHost() {
+        return host;
     }
 }
