00dec6e65d354b98f61431ad436dbc6a4ea79e44
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-07 11:36:06 -0800

    created constructor for "Body.java", still needs logic

--- request/Body.java
+++ request/Body.java
@@ -1,4 +1,14 @@
 package request;
 
+import configuration.headers.Header;
+
+import java.net.Socket;
+
 public class Body {
+
+    private int length;
+
+    public Body(Socket client, Header header) {
+
+    }
 }
