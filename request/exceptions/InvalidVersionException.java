f4afa71ba5f283f7b2d32efa57d5e8c524ccac30
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-11 15:49:25 -0800

    created file

---
+++ request/exceptions/InvalidVersionException.java
@@ -0,0 +1,7 @@
+package request.exceptions;
+
+public class InvalidVersionException extends RequestException {
+    public InvalidVersionException(String message) {
+        super(message);
+    }
+}
