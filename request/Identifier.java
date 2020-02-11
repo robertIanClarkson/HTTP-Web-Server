5b6632e0e71d2d776922154effad97cdcf7c4412
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-11 15:40:56 -0800

    implemented "InvalidIdentifierException"

--- request/Identifier.java
+++ request/Identifier.java
@@ -1,11 +1,20 @@
 package request;
 
+import configuration.Configuration;
+import request.exceptions.InvalidIdentifierException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Identifier {
 
     private String id;
 
-    public Identifier(String id) {
-        this.id = id;
+    public Identifier(String id) throws InvalidIdentifierException {
+        this.id = Configuration.getHttpd().getDocumentRoot() + id;
+        if(Files.notExists(Paths.get(this.id))){
+            throw new InvalidIdentifierException("File \"" + id + "\" does not exist");
+        }
     }
 
     public String getId() {
