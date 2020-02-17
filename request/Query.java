7da11d42c861a1eea8ce33094abdf87a47406a39
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-16 20:06:32 -0800

    Created file

---
+++ request/Query.java
@@ -0,0 +1,14 @@
+package request;
+
+public class Query {
+
+    private String query;
+
+    public Query(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
