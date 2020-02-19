d8ea67f6c5603fd38e96eb171e8f66a252ef1090
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-18 22:43:52 -0800

    more drafting

--- server/accessCheck/AccessCheck.java
+++ server/accessCheck/AccessCheck.java
@@ -3,9 +3,16 @@ package server.accessCheck;
 import server.request.Request;
 
 public class AccessCheck {
+
     public AccessCheck() {}
 
     public static void check(Request request) {
+        if (accessExist(request.getId().getURI())) {
 
+        }
+    }
+
+    private static boolean accessExist(String uri) {
+        return false;
     }
 }
