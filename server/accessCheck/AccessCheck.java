106a9041d09bac1f5fd8039b2842f2ea4cf4bb49
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-18 22:28:36 -0800

    created file

---
+++ server/accessCheck/AccessCheck.java
@@ -0,0 +1,11 @@
+package server.accessCheck;
+
+import server.request.Request;
+
+public class AccessCheck {
+    public AccessCheck() {}
+
+    public static void check(Request request) {
+
+    }
+}
