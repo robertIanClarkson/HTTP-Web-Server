91981aba49ca07ba075a31e8621d613f2866aaa5
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-19 23:06:48 -0800

    Moved logic into seperate methods

--- server/accessCheck/AccessCheck.java
+++ server/accessCheck/AccessCheck.java
@@ -11,16 +11,13 @@ import java.util.HashMap;
 public class AccessCheck {
 
     private HashMap<String, String> directives;
+    private HashMap<String, String> username_password;
 
     public AccessCheck() throws IOException {
-        String line, key, value;
         directives = new HashMap<>();
-        BufferedReader reader = new BufferedReader(new FileReader(Configuration.getHttpd().getAccessFile()));
-        while((line = reader.readLine()) != null) {
-            key = line.substring(0, line.indexOf(" ")).trim();
-            value = stripQuotes(line.substring(line.indexOf(" ")).trim());
-            directives.put(key, value);
-        }
+        username_password = new HashMap<>();
+        setDirectives();
+        setUsernamePasswords();
     }
 
     public static void check(Request request) {
@@ -35,6 +32,26 @@ public class AccessCheck {
 
     private static boolean accessExist(String uri) {
         return false;
+    }
+
+    private void setDirectives() throws IOException {
+        String line, key, value;
+        BufferedReader reader = new BufferedReader(new FileReader(Configuration.getHttpd().getAccessFile()));
+        while((line = reader.readLine()) != null) {
+            key = line.substring(0, line.indexOf(" ")).trim();
+            value = stripQuotes(line.substring(line.indexOf(" ")).trim());
+            directives.put(key, value);
+        }
+    }
+
+    private void setUsernamePasswords() throws IOException {
+        String line, key, value;
+        BufferedReader reader = new BufferedReader(new FileReader(directives.get("AuthUserFile")));
+        while((line = reader.readLine()) != null) {
+            key = line.substring(0, line.indexOf(":")).trim();
+            value = line.substring(line.indexOf(":") + 1).trim();
+            username_password.put(key, value);
+        }
     }
 
     private String stripQuotes(String line) {
