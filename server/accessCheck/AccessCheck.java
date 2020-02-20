4c16dc93879e4bb0b7491dde38daf89bf0a1f233
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-19 17:40:34 -0800

    Captured data from ".htaccess"

--- server/accessCheck/AccessCheck.java
+++ server/accessCheck/AccessCheck.java
@@ -1,10 +1,27 @@
 package server.accessCheck;
 
+import server.configuration.Configuration;
 import server.request.Request;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class AccessCheck {
 
-    public AccessCheck() {}
+    private HashMap<String, String> directives;
+
+    public AccessCheck() throws IOException {
+        String line, key, value;
+        directives = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new FileReader(Configuration.getHttpd().getAccessFile()));
+        while((line = reader.readLine()) != null) {
+            key = line.substring(0, line.indexOf(" ")).trim();
+            value = stripQuotes(line.substring(line.indexOf(" ")).trim());
+            directives.put(key, value);
+        }
+    }
 
     public static void check(Request request) {
         if (accessExist(request.getId().getURI())) {
@@ -12,7 +29,20 @@ public class AccessCheck {
         }
     }
 
+    public String getDirective(String key) {
+        return directives.get(key);
+    }
+
     private static boolean accessExist(String uri) {
         return false;
+    }
+
+    private String stripQuotes(String line) {
+        if(line.contains("\"")) {
+            int x = line.indexOf("\"");
+            int y = line.lastIndexOf("\"");
+            return line.substring( x+1, y);
+        }
+        return line;
     }
 }
