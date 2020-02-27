30634530a9cb6463bd60016f3ce5b87150af48e7
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-02-26 18:46:05 -0800

    Created File

---
+++ server/worker/Sender.java
@@ -0,0 +1,21 @@
+package server.worker;
+
+import server.response.Response;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Sender {
+    static void sendResponse(Socket client, Response response) throws IOException {
+        /* https://stackoverflow.com/questions/1176135/socket-send-and-receive-byte-array */
+        /* https://mkyong.com/java/how-to-send-http-request-getpost-in-java/ */
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        byte[] res = response.toString().getBytes();
+        out.write(res);
+        if(Response.hasBody) {
+            out.write(response.getResponseBody().getBody());
+        }
+        out.close();
+    }
+}
---
+++ server/worker/ServerDebug.java
@@ -0,0 +1,31 @@
+package server.worker;
+
+import server.request.Request;
+import server.response.Response;
+
+public class ServerDebug {
+    static void printRequest(Request request) {
+        System.out.println("------------Request-------------");
+        System.out.println(request);
+        if(Request.hasBody) {
+            System.out.println("***+++HAS BODY+++***");
+//            String body = new String(server.request.getBody().getBody(), StandardCharsets.US_ASCII);
+//            System.out.println(body);
+        }
+    }
+
+    static void printStatusCode(Request request, Response response) {
+        System.out.println("------------Code-------------");
+        System.out.printf("%-3s---> %s\n", response.getCode(), request.getId());
+    }
+
+    static void printResponse(Response response) {
+        System.out.println("------------Response------------");
+        System.out.println(response);
+        if(Response.hasBody) {
+//            System.out.println("***+++HAS BODY+++***");
+//            String body = new String(server.response.getBody().getBody(), StandardCharsets.US_ASCII);
+//            System.out.println(body);
+        }
+    }
+}
