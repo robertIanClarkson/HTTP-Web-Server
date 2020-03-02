d8f3f046749a0dacfd0382e9776508e89a898b3c
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-03-01 23:41:39 -0800

    ResBody : Added second constructor for InputStream

--- server/response/ResBody.java
+++ server/response/ResBody.java
@@ -1,15 +1,18 @@
 package server.response;
 
 /* Used code from: https://stackoverflow.com/questions/858980/file-to-byte-in-java */
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 
 public class ResBody {
 
     private byte[] body;
 
-    public ResBody(String uri) throws IOException {
+    public ResBody(InputStream in) throws IOException { // Script Body
+        body = in.readAllBytes();
+    }
+
+    public ResBody(String uri) throws IOException { // File Body
         File file = new File(uri);
         body = Files.readAllBytes(file.toPath());
     }
