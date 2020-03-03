588ab0a3e3b888f3fe61e0a237c9648f60ebe261
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-03-02 17:35:01 -0800

    Not checking for proper headers anymore

--- server/request/ReqHeaders.java
+++ server/request/ReqHeaders.java
@@ -11,14 +11,6 @@ import java.util.List;
 
 public class ReqHeaders {
 
-    private static List<String> HEADERS = Arrays.asList(
-            "Host", "Connection", "Cache-Control", "DNT", "Upgrade-Insecure-Requests",
-            "User-Agent", "Sec-Fetch-User", "Accept", "Sec-Fetch-Site", "Sec-Fetch-Mode",
-            "Accept-Encoding", "Accept-Language", "Referer", "X-Requested-With",
-            "If-Modified-Since", "If-None-Match", "Pragma", "Authorization",
-            "Content-Length", "Origin", "Content-Type"
-
-    );
     private HashMap<String, String> headers;
 
     public ReqHeaders(BufferedReader reader) throws IOException, BadRequest {
@@ -28,11 +20,7 @@ public class ReqHeaders {
         while(line != null && !(line.equals(""))) {
             key = line.substring(0, line.indexOf(":")).trim();
             value = line.substring(line.indexOf(":") + 1).trim();
-            if(HEADERS.contains(key)) {
-                headers.put(key, value);
-            } else {
-                throw new BadRequest("Invalid HTTP Header: " + line);
-            }
+            headers.put(key, value);
             line = reader.readLine();
         }
     }
